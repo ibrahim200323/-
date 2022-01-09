ea4b1536dfe879fbc0220e3728aa37161d6c33a9
ibrahim200323 <97029870+ibrahim200323@users.noreply.github.com>
2022-01-09 17:00:07 +0300

    add

---
+++ المدعوين.py
@@ -0,0 +1,18 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+inviters=[]
+for d in range(3):
+    inviting=input("enter inivter name :")
+    inviters.__add__(user)
+
+
+allow=input(" choose yes if needing more invitores")
+
+while (allow =="yes"):
+  inviting=input("enter inivter name :")
+  inviters.__add__(user))
+  allow=input(" choose yes if needing more invitores")
+ 
+
+
+print("u have inviting number of person is " len(inviters))
